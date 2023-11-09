8027266bfd1823f3e8e702198781a0a3124d2ae6
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-09 22:19:05 +0100

    Create isocon.sh

---
+++ transcriptome/isocon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name isocon
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --time=48:00:00
+#SBATCH -o isocon.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate thesis
+
+IsoCon pipeline -fl_reads /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/POG_larveIsoSeq_HiFi.fastq -outfolder /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/isocon_assembly
+~                                             
