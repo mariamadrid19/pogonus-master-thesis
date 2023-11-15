9a230556efe3c88ca959048f66d4abb3a9487013
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-15 23:39:45 +0100

    Update 5_scaffolding.sh

--- genome_assembly/5_scaffolding.sh
+++ genome_assembly/5_scaffolding.sh
@@ -7,13 +7,6 @@
 #SBATCH -o scaffolding.%j.out
 #SBATCH -A lp_svbelleghem
 
-cd /scratch/leuven/357/vsc35707/pogonus/yahs
-
-module load SAMtools/1.13-GCC-10.3.0
-module load BWA/0.7.17-GCC-10.3.0
-
-samtools faidx /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa && cut -f1,2 /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa.fai > Pogonus_hifiasm.asm.hic.p_ctg.fa.genome && bwa index Pogonus_hifiasm.asm.hic.p_ctg.fa
-
 yahs Pogonus_hifiasm.asm.hic.p_ctg.fa /scratch/leuven/357/vsc35707/pogonus/mapping/deduplicated_files/Pogonus_chalceus_rep1.bam -o results/POG
 
 #the yahs.out_scaffolds_final.fa file produced here (in the POG directory) is then re-named Pog_2.0.fasta and used to run the ARIMA pipeline again
