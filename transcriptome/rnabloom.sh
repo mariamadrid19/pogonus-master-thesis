ac7624f918f5b8e9657f965b064ffa7c402c6780
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-15 11:16:55 +0100

    Update rnabloom.sh

--- transcriptome/rnabloom.sh
+++ transcriptome/rnabloom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --cluster=genius
 #SBATCH --job-name rnabloom
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=48:00:00
 #SBATCH -o rnabloom.%j.out
 #SBATCH -A lp_svbelleghem
 
 conda activate thesis
 
-rnabloom -long POG_larveIsoSeq_HiFi.fastq -t 30 -lrpb -outdir /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/rnabloom_assembly
+rnabloom -long /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/POG_IsoSeq_HiFi_demux.fastq -lrpb -t 48 -outdir /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/TRANSCRIPTOME_ASSEMBLY/rnabloom_assembly
