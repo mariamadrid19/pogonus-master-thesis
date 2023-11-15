f16f6916372b4b499a1f115f51eb2f02835229e2
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-15 18:08:46 +0100

    Update 2_rnabloom.sh

--- transcriptome/2_rnabloom.sh
+++ transcriptome/2_rnabloom.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name rnabloom
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH -o rnabloom.%j.out
 #SBATCH -A lp_svbelleghem
 
