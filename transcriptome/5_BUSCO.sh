58f716ee7b31cb0bcea4cf93c80ae444938efa84
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-15 11:37:25 +0100

    Create 5_BUSCO.sh

---
+++ transcriptome/5_BUSCO.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name rnabloom
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH -o rnabloom.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate thesis #this is where MetaEuk is installed)
+module load BUSCO/3.0.2-foss-2018a-Python-2.7.14
+module load HMMER/3.2.1-foss-2018a
+
+busco --download-mode fast_lineage -l insecta -o insecta_lineage
+
+busco -i assembled-transcriptome.fasta -l insecta -o busco_output  -m transcriptome
