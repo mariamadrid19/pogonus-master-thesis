91e35bd26c57a64760683d5ca4f82a90c04b8570
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-15 12:06:15 +0100

    Update 3_hifiasm.sh

--- genome_assembly/3_hifiasm.sh
+++ genome_assembly/3_hifiasm.sh
@@ -7,10 +7,10 @@
 #SBATCH -o hifiasm.%j.out
 #SBATCH -A lp_svbelleghem
 
-module load hifiasm 
+cd /scratch/leuven/357/vsc35707/pogonus/hifiasm
 
 #assembly
-hifiasm -o Pogonus_hifiasm.asm --n-hap 2 --hom-cov 50 -t 20 --h1 GC143248_ACTCTCGA-TGGTACAG_S65_L001_R1_001.fastq.gz --h2 GC143248_ACTCTCGA-TGGTACAG_S65_L001_R2_001.fastq.gz Pogonus.ccs.fastq
+hifiasm -o Pogonus_hifiasm.asm --n-hap 2 --hom-cov 50 -t 32 --h1 GC143248_ACTCTCGA-TGGTACAG_S65_L001_R1_001.fastq.gz --h2 GC143248_ACTCTCGA-TGGTACAG_S65_L001_R2_001.fastq.gz HiFi-reads.fq.gz
 
 #gfa_to_fasta
 awk '/^S/{print ">"$2"\n"$3}' Pogonus_hifiasm.asm.hic.p_ctg.gfa | fold > Pogonus_hifiasm.asm.hic.p_ctg.fa
