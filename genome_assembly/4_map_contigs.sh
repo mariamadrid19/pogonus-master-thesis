097c9e3ff727e1d231b7b05fe9693958bf76da2a
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-17 15:19:26 +0100

    Update 4_map_contigs.sh

--- genome_assembly/4_map_contigs.sh
+++ genome_assembly/4_map_contigs.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -l
 #SBATCH --cluster=genius
-#SBATCH --job-name mapping_pipeline
+#SBATCH --job-name map_contigs
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=12
 #SBATCH --time=36:00:00
-#SBATCH -o mapping.%j.out
+#SBATCH -o map_contigs.%j.out
 #SBATCH -A lp_svbelleghem
 
 # This pipeline is modified from ArimaGenomics (arima_mapping_pipeline.sh)
@@ -15,7 +15,7 @@
 ##############################################
 
 # Below find the commands used to map HiC data to the contigs (output from hifiasm)
-# This bash script will map one paired end HiC dataset (read1 & read2 FASTQs). Feel to modify and multiplex as you see fit to work with your volume of samples and system.
+# This bash script will map one paired end HiC dataset (R1 & R2) to the contigs produced by hifiasm (Pogonus_hifiasm.asm.hic.p_ctg.fa)
 
 ##########################################
 # Commands #
@@ -27,16 +27,16 @@ IN_DIR='/scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_OMNI_C/Sample_DUDZELE
 REF='/scratch/leuven/357/vsc35707/pogonus/yahs/Pogonus_hifiasm.asm.hic.p_ctg.fa'
 FAIDX='$REF.fai'
 PREFIX='bwa_Pogonus'
-RAW_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/bams'
-FILT_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/filtered_bams'
-FILTER='/scratch/leuven/357/vsc35707/pogonus/mapping/filter_five_end.pl'
-COMBINER='/scratch/leuven/357/vsc35707/pogonus/mapping/two_read_bam_combiner.pl'
-STATS='/scratch/leuven/357/vsc35707/pogonus/mapping/get_stats.pl'
-TMP_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/temporary_files'
-PAIR_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/paired_bams'
-REP_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/deduplicated_files'
+RAW_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/bams'
+FILT_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/filtered_bams'
+FILTER='/scratch/leuven/357/vsc35707/pogonus/map_contigs/filter_five_end.pl'
+COMBINER='/scratch/leuven/357/vsc35707/pogonus/map_contigs/two_read_bam_combiner.pl'
+STATS='/scratch/leuven/357/vsc35707/pogonus/map_contigs/get_stats.pl'
+TMP_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/temporary_files'
+PAIR_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/paired_bams'
+REP_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/deduplicated_files'
 REP_LABEL=${LABEL}_rep1
-MERGE_DIR='/scratch/leuven/357/vsc35707/pogonus/mapping/final_merged_alignments'
+MERGE_DIR='/scratch/leuven/357/vsc35707/pogonus/map_contigs/final_merged_alignments'
 MAPQ_FILTER=10
 CPU=12
 
@@ -77,19 +77,6 @@ perl $COMBINER $FILT_DIR/${SRA}_1.bam $FILT_DIR/${SRA}_2.bam samtools $MAPQ_FILT
 echo "### Step 3.B: Add read group"
 java -Xmx4G -Djava.io.tmpdir=temp/ -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups INPUT=$TMP_DIR/$SRA.bam OUTPUT=$PAIR_DIR/$SRA.bam ID=$SRA LB=$SRA SM=$LABEL PL=ILLUMINA PU=none
 
-###############################################################################################################################################################
-###                                           How to Accommodate Technical Replicates                                                                       ###
-### This pipeline is currently built for processing a single sample with one read1 and read2 FASTQ file.                                                    ###
-### Technical replicates (eg. one library split across multiple lanes) should be merged before running the MarkDuplicates command.                          ###
-### If this step is run, the names and locations of input files to subsequent steps will need to be modified in order for subsequent steps to run correctly.###
-### The code below is an example of how to merge technical replicates.                                                                                      ###
-###############################################################################################################################################################
-#	REP_NUM=X # number of the technical replicate set e.g. 1
-#	REP_LABEL=${LABEL}_rep$REP_NUM
-#	INPUTS_TECH_REPS=('bash' 'array' 'of' 'bams' 'from' 'replicates') # BAM files you want combined as technical replicates
-#   example bash array - INPUTS_TECH_REPS=('INPUT=A.L1.bam' 'INPUT=A.L2.bam' 'INPUT=A.L3.bam')
-#	java -Xmx8G -Djava.io.tmpdir=temp/ -jar $PICARD MergeSamFiles $INPUTS_TECH_REPS OUTPUT=$TMP_DIR/$REP_LABEL.bam USE_THREADING=TRUE ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=LENIENT
-
 echo "### Step 4: Mark duplicates"
 java -Xmx30G -XX:-UseGCOverheadLimit -Djava.io.tmpdir=temp/ -jar $EBROOTPICARD/picard.jar MarkDuplicates INPUT=$PAIR_DIR/$SRA.bam OUTPUT=$REP_DIR/$REP_LABEL.bam METRICS_FILE=$REP_DIR/metrics.$REP_LABEL.txt TMP_DIR=$TMP_DIR ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=LENIENT REMOVE_DUPLICATES=TRUE
 
@@ -98,21 +85,3 @@ samtools index $REP_DIR/$REP_LABEL.bam
 perl $STATS $REP_DIR/$REP_LABEL.bam > $REP_DIR/$REP_LABEL.bam.stats
 
 echo "Finished Mapping Pipeline through Duplicate Removal"
-
-#########################################################################################################################################
-###                                       How to Accommodate Biological Replicates                                                    ###
-### This pipeline is currently built for processing a single sample with one read1 and read2 FASTQ file.                              ###
-### Biological replicates (eg. multiple libraries made from the same sample) should be merged before proceeding with subsequent steps.###
-### The code below is an example of how to merge biological replicates.                                                               ###
-#########################################################################################################################################
-#
-#	INPUTS_BIOLOGICAL_REPS=('bash' 'array' 'of' 'bams' 'from' 'replicates') # BAM files you want combined as biological replicates
-#   example bash array - INPUTS_BIOLOGICAL_REPS=('INPUT=A_rep1.bam' 'INPUT=A_rep2.bam' 'INPUT=A_rep3.bam')
-#
-#	java -Xmx8G -Djava.io.tmpdir=temp/ -jar $PICARD MergeSamFiles $INPUTS_BIOLOGICAL_REPS OUTPUT=$MERGE_DIR/$LABEL.bam USE_THREADING=TRUE ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=LENIENT
-#
-#	samtools index $MERGE_DIR/$LABEL.bam
-
-# perl $STATS $MERGE_DIR/$LABEL.bam > $MERGE_DIR/$LABEL.bam.stats
-
-# echo "Finished Mapping Pipeline through merging Biological Replicates"
