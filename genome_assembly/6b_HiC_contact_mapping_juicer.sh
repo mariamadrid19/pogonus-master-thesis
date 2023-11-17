78b08e0c4c16a510c1bc423ca01f85568242ce93
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-17 14:01:37 +0100

    Update 6b_HiC_contact_mapping_juicer.sh

--- genome_assembly/6b_HiC_contact_mapping_juicer.sh
+++ genome_assembly/6b_HiC_contact_mapping_juicer.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name mapping_pipeline_juicer
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=12
-#SBATCH --time=36:00:00
+#SBATCH --time=72:00:00
 #SBATCH -o juicer.%j.out
 #SBATCH -A lp_svbelleghem
 
@@ -12,3 +12,5 @@ conda activate thesis
 cd /scratch/leuven/357/vsc35707/pogonus/yahs
 
 (juicer pre yahs.out.bin yahs.out_scaffolds_final.agp Pogonus_hifiasm.asm.hic.p_ctg.fa.fai | sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G | awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
+
+java -jar -Xmx32G juicer_tools.1.9.9_jcuda.0.8.jar pre alignments_sorted.txt out.hic.part scaffolds_final.chrom.sizes) && (mv out.hic.part out.hic)
