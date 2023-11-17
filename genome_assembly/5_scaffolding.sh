d5075af49e2057e027b256d0f900142e79408b66
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-17 13:48:31 +0100

    Update 5_scaffolding.sh

--- genome_assembly/5_scaffolding.sh
+++ genome_assembly/5_scaffolding.sh
@@ -12,9 +12,8 @@ yahs Pogonus_hifiasm.asm.hic.p_ctg.fa /scratch/leuven/357/vsc35707/pogonus/mappi
 #the yahs.out_scaffolds_final.fa file produced here (in the POG directory) is then re-named Pog_2.0.fasta and used to run the ARIMA pipeline again
 #this time, the mapping will be using the Pog_2.0.fasta as the reference (instead of the hifiasm assembly)
 
-cp POG/yahs.out_scaffolds_final.fa /scratch/leuven/357/vsc35707/pogonus/mapping_HiC
-cd /scratch/leuven/357/vsc35707/pogonus/mapping_HiC
-mv yahs.out_scaffolds_final.fa Pog_2.0.fa
+mv POG/yahs.out_scaffolds_final.fa POG/Pog_2.0.fa
+cp POG/Pog_2.0.fa /scratch/leuven/357/vsc35707/pogonus/mapping_HiC
 
 #finally, the newly re-named assembly is also indexed in order to run the ARIMA pipeline once more, this time mapping to this scaffolding assembly
 samtools faidx Pog_2.0.fa && cut -f1,2 Pog_2.0.fa.fai > Pog_2.0.fa.genome && bwa index Pog_2.0.fa
