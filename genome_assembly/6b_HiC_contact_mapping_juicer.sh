2ec0d8716552a46fe2548c301631d288a7f38358
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-17 13:49:59 +0100

    Update 6b_HiC_contact_mapping_juicer.sh

--- genome_assembly/6b_HiC_contact_mapping_juicer.sh
+++ genome_assembly/6b_HiC_contact_mapping_juicer.sh
@@ -10,4 +10,5 @@
 conda activate thesis
 
 cd /scratch/leuven/357/vsc35707/pogonus/yahs
-juicer pre yahs.out.bin yahs.out_scaffolds_final.agp Ogibo_hifiasm2_4hap.asm.hic.p_ctg.fa.fai | sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G | awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt
+
+(juicer pre yahs.out.bin yahs.out_scaffolds_final.agp Pogonus_hifiasm.asm.hic.p_ctg.fa.fai | sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G | awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
