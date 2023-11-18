2a79b7a3b626f9affa41c1ee62be9b743eb61764
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-18 23:39:57 +0100

    Update and rename 5_isoseq3.sh to 3_isoseq3.sh

--- transcriptome/3_isoseq3.sh
+++ transcriptome/3_isoseq3.sh
@@ -2,7 +2,7 @@
 #SBATCH --cluster=genius
 #SBATCH --job-name isoseq3
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=72:00:00
 #SBATCH -o isoseq3.%j.out
 #SBATCH -A lp_svbelleghem
@@ -11,5 +11,7 @@ conda activate thesis
 
 #clustering step
 isoseq3 cluster POG_larveIsoSeq.demux.bam POG_larveIsoSeq.unpolished.bam --verbose
+#polishing step is not necessary (subreads are not available)
 
-#polishing step is not necessary
+isoseq3 summarize POG_larveIsoSeq.unpolished.bam summary-isoseq3.csv
+
