93f54d4b4fc1226978c5e7dcac89e1db2a4f1aaf
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-18 22:56:35 +0100

    Create sortmerna.sh

---
+++ transcriptome/sortmerna.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name sortmerna
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=72:00:00
+#SBATCH -A lp_svbelleghem
+#SBATCH -o sortmerna.%j.out
+
+conda activate thesis
+cd /scratch/leuven/357/vsc35707/pogonus/reads/Pogonus_PACBIO_RNA/rRNA
+
+sortmerna --ref silva_rfam_databases/rfam-5.8s-database-id98.fasta --reads POG_IsoSeq_HiFi_demux.fasta
