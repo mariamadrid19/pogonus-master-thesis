cd792b7f9b1bbb62f68f91c0e5df9232709bb468
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-21 20:24:29 +0100

    Update 6_BUSCO.sh

--- transcriptome/6_BUSCO.sh
+++ transcriptome/6_BUSCO.sh
@@ -2,7 +2,7 @@
 #SBATCH --cluster=genius
 #SBATCH --job-name BUSCO
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=72:00:00
 #SBATCH -o busco.%j.out
 #SBATCH -A lp_svbelleghem
@@ -16,4 +16,4 @@ sed -i 's/\//|/g' POG_larveIsoSeq.unpolished.hq.fasta
 cd /scratch/pogonus/reads/Pogonus_PACBIO_RNA/TRANSCRIPTOME_ASSEMBLY/BUSCO/
 
 #only works with de-compressed, clean fasta files (headers must be free of "/")
-busco -i POG_larveIsoSeq.unpolished.hq.fasta -l insecta_odb10 -o output  -m transcriptome
+busco -i POG_larveIsoSeq.unpolished.hq.fasta -l insecta_odb10 -o output  -m transcriptome  -c 32
