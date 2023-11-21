16013cbd757c1f4d5f0e155c164fc224af90c4b9
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-21 20:32:12 +0100

    Update 7_rnaQUAST.sh

--- transcriptome/7_rnaQUAST.sh
+++ transcriptome/7_rnaQUAST.sh
@@ -2,7 +2,7 @@
 #SBATCH --cluster=genius
 #SBATCH --job-name rnaQUAST
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=72:00:00
 #SBATCH -o rnaQUAST.%j.out
 #SBATCH -A lp_svbelleghem
@@ -13,11 +13,19 @@ conda activate thesis
 
 #https://cyverse.atlassian.net/wiki/spaces/TUT/pages/258736197/rnaQUAST+1.2.0+denovo+based+using+DE
 
-rnaQUAST.py --transcripts /PATH/TO/rnabloom_assembly.fasta /PATH/TO/isoscon_assembly.fasta /PATH/TO/isoseq3_assembly.fasta \
+rnaQUAST.py --transcripts /scratch/pogonus/reads/Pogonus_PACBIO_RNA/TRANSCRIPTOME_ASSEMBLY/BUSCO/ \
 --busco insecta_lineage \
---o rnaQUAST_output_BUSCO
+--o rnaQUAST_output_BUSCO 
 
-rnaQUAST.py --transcripts /PATH/TO/rnabloom_assembly.fasta /PATH/TO/isoscon_assembly.fasta /PATH/TO/isoseq3_assembly.fasta \
+rnaQUAST.py --transcripts /scratch/pogonus/reads/Pogonus_PACBIO_RNA/TRANSCRIPTOME_ASSEMBLY/BUSCO/ \
 --gene_mark \
 --o rnaQUAST_output_GM
+
+#rnaQUAST.py --transcripts /PATH/TO/rnabloom_assembly.fasta /PATH/TO/isoscon_assembly.fasta /PATH/TO/isoseq3_assembly.fasta \
+#--busco insecta_lineage \
+#--o rnaQUAST_output_BUSCO
+
+#rnaQUAST.py --transcripts /PATH/TO/rnabloom_assembly.fasta /PATH/TO/isoscon_assembly.fasta /PATH/TO/isoseq3_assembly.fasta \
+#--gene_mark \
+#--o rnaQUAST_output_GM
 
