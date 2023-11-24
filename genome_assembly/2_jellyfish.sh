4cf89414acf82eed2cdeb5cee64fc7fe9a5f4b1c
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-24 13:41:48 +0100

    Create 2_jellyfish.sh

---
+++ genome_assembly/2_jellyfish.sh
@@ -0,0 +1,19 @@
+!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name jellyfish
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=12:00:00
+#SBATCH -o jellyfish.%j.out
+#SBATCH -A lp_svbelleghem
+
+
+module load Jellyfish/2.2.10-intel-2018a
+
+jellyfish count -m 21 -s 100M -t 20 -C hifi_reads.fasta
+
+jellyfish histo mer_counts.jf
+
+jellyfish dump mer_counts.jf > mer_counts_dumps.fa
+
+jellyfish info mer_counts.jf
