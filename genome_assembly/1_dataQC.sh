efe98bab1ddbd9ecb1b96d72eefc2fa0015b4fd3
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-29 19:23:20 +0100

    Update 1_dataQC.sh

--- genome_assembly/1_dataQC.sh
+++ genome_assembly/1_dataQC.sh
@@ -7,6 +7,10 @@
 #SBATCH -o fastqc.%j.out
 #SBATCH -A lp_svbelleghem
 
+conda activate thesis
+
+bam2fastq -o POG_HiFi_reads m64279e_231107_135307.reads.bam
+
 module load FastQC/0.11.8-Java-1.8.0_162
 
 fastqc POG_HiFi_reads.fastq.gz
