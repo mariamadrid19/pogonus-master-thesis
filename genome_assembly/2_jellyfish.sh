af279bb492298ae93b536f11ca91e48f8887ce02
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-29 22:58:54 +0100

    Update 2_jellyfish.sh

--- genome_assembly/2_jellyfish.sh
+++ genome_assembly/2_jellyfish.sh
@@ -7,6 +7,8 @@
 #SBATCH -o jellyfish.%j.out
 #SBATCH -A lp_svbelleghem
 
+cd seqtk/
+seqtk seq -a /scratch/leuven/357/vsc35707/pogonus/fastqc/POG_HiFi_reads.fastq.gz > POG_HiFi_reads.fasta
 
 module load Jellyfish/2.2.10-intel-2018a
 
