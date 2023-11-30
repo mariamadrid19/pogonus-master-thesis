797367610ecfd8e9f15077a1c2f308fa594e40b9
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-30 10:26:47 +0100

    Update 2_jellyfish.sh

--- genome_assembly/2_jellyfish.sh
+++ genome_assembly/2_jellyfish.sh
@@ -2,7 +2,7 @@
 #SBATCH --cluster=genius
 #SBATCH --job-name jellyfish
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=12:00:00
 #SBATCH -o jellyfish.%j.out
 #SBATCH -A lp_svbelleghem
@@ -12,7 +12,7 @@ seqtk seq -a /scratch/leuven/357/vsc35707/pogonus/fastqc/POG_HiFi_reads.fastq.gz
 
 module load Jellyfish/2.2.10-intel-2018a
 
-jellyfish count -m 21 -s 100M -t 20 -C POG_HiFi_reads.fasta
+jellyfish count -m 21 -s 100M -t 32 -C POG_HiFi_reads.fasta
 
 jellyfish histo mer_counts.jf
 
