00417bcd776de47fd2ab5ce13216241993c6ebd4
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-30 11:02:48 +0100

    Update 1_dataQC.sh

--- genome_assembly/1_dataQC.sh
+++ genome_assembly/1_dataQC.sh
@@ -12,8 +12,10 @@ conda activate thesis
 #this will generate a HiFi-only BAM from a reads.bam file since the current reads.bam file still has the many copies of each fragment that result from the circular sequencing
 extracthifi m64279e_231107_135307.reads.bam POG_HiFi_reads.bam
 
+#this will generate a fastq.gz file from the hifi_reads.bam file
 bam2fastq -o POG_HiFi_reads POG_HiFi_reads.bam
 
 module load FastQC/0.11.8-Java-1.8.0_162
 
+#this will run fastqc on the fastq.gz HiFi reads
 fastqc POG_HiFi_reads.fastq.gz -t 32
