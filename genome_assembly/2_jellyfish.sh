046838d799d49a308abe556b2ff274cedce38dff
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-11-30 12:56:43 +0100

    Update 2_jellyfish.sh

--- genome_assembly/2_jellyfish.sh
+++ genome_assembly/2_jellyfish.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name jellyfish
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=32
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH -o jellyfish.%j.out
 #SBATCH -A lp_svbelleghem
 
@@ -13,11 +13,13 @@ seqtk seq -a /scratch/leuven/357/vsc35707/pogonus/fastqc/POG_HiFi_reads.fastq.gz
 
 module load Jellyfish/2.2.10-intel-2018a
 
-#this will count kmers on the fasta file 
+#this will count kmers on the fasta file, takes a long time
 jellyfish count -m 21 -s 100M -t 32 -C POG_HiFi_reads.fasta
 
+#this makes the k-mer histogram (can be seen on the .out file, DO NOT DELETE IT)
 jellyfish histo mer_counts.jf
 
-jellyfish dump mer_counts.jf > mer_counts_dumps.fa
+#this generates an incredibly big file! be careful with storage
+#jellyfish dump mer_counts.jf > mer_counts_dumps.fa
 
 jellyfish info mer_counts.jf
