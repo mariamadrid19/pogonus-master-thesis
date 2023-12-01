50c8d145f6e4dc3542af81e652d3e8df638d5c76
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-12-01 21:24:20 +0100

    Create 6_isONclust.sh

---
+++ transcriptome/6_isONclust.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name isonclust
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=72:00:00
+#SBATCH -o isonclust.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate thesis
+
+#clustering step
+isONclust --isoseq --fastq POG_IsoSeq_HiFi_demux.fastq --outfolder TRANSCRIPTOME_ASSEMBLY/isONclust/
