5707a9a66decda57397dc8d6afb0046032cf1b43
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-12-01 16:54:03 +0100

    Update and rename 5_compleasm.sh to 5_completeness.sh

--- genome_assembly/5_completeness.sh
+++ genome_assembly/5_completeness.sh
@@ -7,4 +7,6 @@
 #SBATCH -o compleasm.%j.out
 #SBATCH -A lp_svbelleghem
 
-python compleasm.py run -a /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -o results/ -l insecta_odb10 -t 32
+conda activate compleasm 
+
+compleasm run -a /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -o results/ -l insecta_odb10 -t 32
