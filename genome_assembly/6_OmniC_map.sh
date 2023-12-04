11988d075e28799f352b7826a955b4571ec4dc43
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-12-04 18:05:35 +0100

    Update 6_OmniC_map.sh

--- genome_assembly/6_OmniC_map.sh
+++ genome_assembly/6_OmniC_map.sh
@@ -12,7 +12,7 @@ conda activate thesis
 bwa mem -5SP -T0 -t 16 /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa /scratch/leuven/357/vsc35707/pogonus/hifiasm/GC143248_ACTCTCGA-TGGTACAG_S65_R1.fastq /scratch/leuven/357/vsc35707/pogonus/hifiasm/GC143248_ACTCTCGA-TGGTACAG_S65_R2.fastq | \
 pairtools parse --min-mapq 40 --walks-policy 5unique \
 --max-inter-align-gap 30 --nproc-in 16 --nproc-out 16 --chroms-path /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa.genome | \
-pairtools sort --tmpdir=/Omni_C/temp/ --nproc 12|pairtools dedup --nproc-in 16 \
+pairtools sort --tmpdir=/temp/ --nproc 12|pairtools dedup --nproc-in 16 \
 --nproc-out 16 --mark-dups --output-stats stats.txt|pairtools split --nproc-in 16 \
 --nproc-out 16 --output-pairs mapped.pairs --output-sam -|samtools view -bS -@16 | \
 samtools sort -@16 -o mapped.PT.bam;samtools index mapped.PT.bam
