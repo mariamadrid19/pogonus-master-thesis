44d66d9ee1e5186dcce36310f6d5e09c2bf96ebe
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-12-04 18:40:43 +0100

    Update 7_scaffolding.sh

--- genome_assembly/7_scaffolding.sh
+++ genome_assembly/7_scaffolding.sh
@@ -12,14 +12,12 @@ source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
 conda activate thesis 
 
 #YaHs will take the contig sequences (.fa) and the HiC aligned to the contigs (.bam produced in step 4) and scaffold them 
-yahs /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa /scratch/leuven/357/vsc35707/pogonus/map_contigs/deduplicated_files/Pogonus_chalceus_r.bam -o Pog_2.0
+yahs /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa /scratch/leuven/357/vsc35707/pogonus/map_contigs/deduplicated_files/Pogonus_chalceus_r.bam
 
-#Pog_2.0.fa is used to run the ARIMA pipeline again, the mapping will be using the scaffolds as the reference (instead of the contigs)
-
-mv Pog_2.0_scaffolds_final.fa Pog_2.0.fa
+#final scaffolds (.fa) are used to run the ARIMA pipeline again, the mapping will be using the scaffolds as the reference (instead of the contigs)
 
 module load SAMtools/1.13-GCC-10.3.0
 
 #finally, the newly re-named assembly is also indexed in order to run the ARIMA pipeline once more, this time mapping to this scaffolding assembly
-samtools faidx Pog_2.0.fa && cut -f1,2 Pog_2.0.fa.fai > Pog_2.0.fa.genome
+samtools faidx yahs.out_scaffolds_final.fa && cut -f1,2 yahs.out_scaffolds_final.fai > yahs.out_scaffolds_final.fa.genome
 
