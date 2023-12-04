002c743a92082e52ac005fbdb795847b3754a308
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-12-04 18:27:11 +0100

    Update 9b_HiC_contact_mapping_juicer.sh

--- genome_assembly/9b_HiC_contact_mapping_juicer.sh
+++ genome_assembly/9b_HiC_contact_mapping_juicer.sh
@@ -9,8 +9,8 @@
 
 conda activate thesis
 
-cd /scratch/leuven/357/vsc35707/pogonus/yahs
+juicer pre yahs.out.bin yahs.out_scaffolds_final.agp /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa.fai | \ 
+sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G | awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
 
-(juicer pre yahs.out.bin yahs.out_scaffolds_final.agp Pogonus_hifiasm.asm.hic.p_ctg.fa.fai | sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G | awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
 
-java -jar -Xmx32G juicer_tools.1.9.9_jcuda.0.8.jar pre alignments_sorted.txt out.hic.part scaffolds_final.chrom.sizes) && (mv out.hic.part out.hic)
+(java -jar -Xmx32G juicer_tools.1.9.9_jcuda.0.8.jar pre alignments_sorted.txt out.hic.part scaffolds_final.chrom.sizes) && (mv out.hic.part out.hic)
