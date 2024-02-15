3fc490d7cee33435025be4250a2e1a5243c98760
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-02-15 15:20:04 +0100

    Update 3_hicanu.sh

--- genome_assembly/3_hicanu.sh
+++ genome_assembly/3_hicanu.sh
@@ -7,6 +7,12 @@
 #SBATCH -o hicanu.%j.out
 #SBATCH -A lp_svbelleghem
 
+#Canu installation
+#curl -L https://github.com/marbl/canu/releases/download/v2.2/canu-2.2.tar.xz --output canu-2.2.tar.xz
+#tar -xJf canu-2.2.tar.xz
+#cd canu-2.2/src
+#make -j 8
+
 export PATH=$PATH:/scratch/leuven/357/vsc35707/dudzele_pogonus/canu-2.2/build/bin/
 
 canu -p hifi_canu_pogonus -d canu_results/ genomeSize=444m -pacbio-hifi /scratch/leuven/357/vsc35707/pogonus/hifiasm/POG_HiFi_reads.fastq.gz 
