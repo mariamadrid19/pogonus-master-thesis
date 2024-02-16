67b824d706a02087dbe73e82e8349df216ff04b5
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-02-16 16:45:04 +0100

    Update 5_completeness.sh

--- genome_assembly/5_completeness.sh
+++ genome_assembly/5_completeness.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 #SBATCH --cluster=genius
-#SBATCH --job-name compleasm
+#SBATCH --job-name compleasm_quast
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=24:00:00
-#SBATCH -o compleasm.%j.out
+#SBATCH -o compleasm_quast.%j.out
 #SBATCH -A lp_svbelleghem
 
 conda activate compleasm 
 
-compleasm run -a /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -o results/ -l insecta_odb10 -t 32
+compleasm run -a /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -o results/ -l insecta_odb10 -t 32
 
 conda activate thesis
 
-quast /scratch/leuven/357/vsc35707/pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -t 12
+quast /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/Pogonus_hifiasm.asm.hic.p_ctg.fa -t 12
