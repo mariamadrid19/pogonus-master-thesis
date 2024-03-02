e2966d8ffae0d6d7a6f58099b4d120a794d9549e
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-02 12:01:46 +0100

    Update 3_hicanu.sh

--- genome_assembly/3_hicanu.sh
+++ genome_assembly/3_hicanu.sh
@@ -3,9 +3,11 @@
 #SBATCH --job-name assemble_canu
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=32
-#SBATCH --time=72:00:00
+#SBATCH --time=80:00:00
 #SBATCH -o hicanu.%j.out
 #SBATCH -A lp_svbelleghem
+#SBATCH --partition=bigmem
+#SBATCH --mem-per-cpu=28000M
 
 #Canu installation
 #curl -L https://github.com/marbl/canu/releases/download/v2.2/canu-2.2.tar.xz --output canu-2.2.tar.xz
@@ -13,6 +15,6 @@
 #cd canu-2.2/src
 #make -j 8
 
-export PATH=$PATH:/scratch/leuven/357/vsc35707/dudzele_pogonus/canu-2.2/build/bin/
+#export PATH=$PATH:/scratch/leuven/357/vsc35707/dudzele_pogonus/canu-2.2/build/bin/
 
-canu -p hifi_canu_pogonus -d canu_results/ genomeSize=444m -pacbio-hifi /scratch/leuven/357/vsc35707/pogonus/hifiasm/POG_HiFi_reads.fastq.gz 
+canu -p hifi_canu_pogonus -d canu_results/ genomeSize=1300M -pacbio-hifi /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/POG_HiFi_reads.fastq 
