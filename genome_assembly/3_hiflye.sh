dfe184b4792179e17140ada742d0641a4298ab68
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-04 16:17:13 +0100

    Update 3_hiflye.sh

--- genome_assembly/3_hiflye.sh
+++ genome_assembly/3_hiflye.sh
@@ -7,4 +7,6 @@
 #SBATCH -o hiflye.%j.out
 #SBATCH -A lp_svbelleghem
 
+conda activate thesis
+
 flye --pacbio-hifi /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/POG_HiFi_reads.fastq --genome-size 1.3g --threads 32
