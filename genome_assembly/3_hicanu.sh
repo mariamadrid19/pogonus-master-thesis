dd4c5eecca3d692c62f5c8ab489c49f2b6559db2
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-04 16:15:42 +0100

    Update 3_hicanu.sh

--- genome_assembly/3_hicanu.sh
+++ genome_assembly/3_hicanu.sh
@@ -5,6 +5,6 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=72:00:00
 #SBATCH -o hicanu.%j.out
-#SBATCH -A lp_edu_comparativegenomics
+#SBATCH -A lp_svbelleghem
 
 canu -p hifi_canu_pogonus useGrid=false -d canu_results/ genomeSize=1300M -pacbio-hifi /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/POG_HiFi_reads.fastq
