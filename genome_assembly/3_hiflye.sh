3a232d986553bab541b06688de5d365b20e0719c
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-04 16:15:34 +0100

    Create 3_hiflye.sh

---
+++ genome_assembly/3_hiflye.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name assemble_flye
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=72:00:00
+#SBATCH -o hiflye.%j.out
+#SBATCH -A lp_svbelleghem
+
+flye --pacbio-hifi /scratch/leuven/357/vsc35707/dudzele_pogonus/hifiasm/POG_HiFi_reads.fastq --genome-size 1.3g --threads 32
