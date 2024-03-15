2e19779f135d113f500fd852f4c86ef4238091c7
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-15 15:50:30 +0100

    Update 5_ntSynt.sh

--- troubleshooting/5_ntSynt.sh
+++ troubleshooting/5_ntSynt.sh
@@ -4,13 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
-#SBATCH -o ntsynt.%j.out
+#SBATCH -o ntsynt_all.%j.out
 #SBATCH -A lp_svbelleghem
 
-conda activate ntsynt
+conda activate ntsynt 
 
 #Multi-genome synteny detection using minimizer graphs
-ntSynt dh1_50_scaffolds.fasta sorted_nh1.fasta -p ragtag_dh1_nh1 -d 20
+ntSynt -d 20 dh1_50_scaffolds.fasta sorted_dh2.fasta sorted_nh1.fasta sorted_nh2.fasta -p synteny_all_haps -t 24 
 
 # Sort the blocks based on the specified order
 python sort_ntsynt_blocks.py --synteny_blocks ragtag_dh1_nh1.synteny_blocks.tsv --sort_order dh1_50_scaffolds.fasta.fai  sorted_nh1.fasta.fai --fais > ragtag_dh1_nh1.synteny_blocks.sorted.tsv
