a3b4907de96ee241495c03c5e58dc47c370beed6
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-18 21:49:36 +0100

    Update 5_ntSynt.sh

--- troubleshooting/5_ntSynt.sh
+++ troubleshooting/5_ntSynt.sh
@@ -18,8 +18,8 @@ python sort_ntsynt_blocks.py --synteny_blocks synteny_all_haps.synteny_blocks.ts
 # Generate the files needed for plotting with gggenomes
 python format_blocks_gggenomes.py --fai dh1_50_scaffolds.fasta.fai sorted_dh1.fasta.fai sorted_nh2.fasta.fai sorted_nh1.fasta.fai --prefix synteny_all_haps --blocks synteny_all_haps.synteny_blocks.sorted.tsv --length 100 --colour dh1_50_scaffolds.fasta
 
-cat ragtag_dh1_nh1.links.tsv  | mlr --tsv sort -f strand -n block_id > ragtag_dh1_nh1.links.sorted.tsv && mv ragtag_dh1_nh1.links.sorted.tsv ragtag_dh1_nh1.links.tsv
-cat ragtag_dh1_nh1.sequence_lengths.tsv | mlr --tsv sort -f seq_id > ragtag_dh1_nh1.sequence_lengths.sorted.tsv && mv ragtag_dh1_nh1.sequence_lengths.sorted.tsv ragtag_dh1_nh1.sequence_lengths.tsv
+cat synteny_all_haps.synteny_blocks.links.tsv  | mlr --tsv sort -f strand -n block_id > synteny_all_haps.synteny_blocks.links.sorted.tsv && mv synteny_all_haps.synteny_blocks.sorted.tsv synteny_all_haps.synteny_blocks.links.tsv
+cat synteny_all_haps.synteny_blocks.sequence_lengths.tsv | mlr --tsv sort -f seq_id > synteny_all_haps.synteny_blocks.sequence_lengths.sorted.tsv && mv synteny_all_haps.synteny_blocks.sequence_lengths.sorted.tsv synteny_all_haps.synteny_blocks.sequence_lengths.tsv
 
 #run R script to generate plots
 Rscript plot_synteny_blocks_gggenomes.R -s ragtag_dh1_nh1.sequence_lengths.tsv -l ragtag_dh1_nh1.links.tsv --scale 20000000 --p ragtag_dh1_nh1
