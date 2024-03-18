8ea18accdf0ec819d7ef36142e3a03244fb1e14a
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-03-19 00:04:08 +0100

    Update 5_ntSynt.sh

--- troubleshooting/5_ntSynt.sh
+++ troubleshooting/5_ntSynt.sh
@@ -9,17 +9,16 @@
 
 conda activate ntsynt 
 
-#Multi-genome synteny detection using minimizer graphs
-ntSynt -d 20 dh1_50_scaffolds.fasta sorted_dh2.fasta sorted_nh1.fasta sorted_nh2.fasta -p synteny_all_haps -t 24 
+ntSynt dh1_50_scaffolds.fasta sorted_dh2.fasta sorted_nh1.fasta sorted_nh2.fasta -p synteny_all_haps -t 24 -d 30
 
-# Sort the blocks based on the specified order
-python sort_ntsynt_blocks.py --synteny_blocks synteny_all_haps.synteny_blocks.tsv --sort_order dh1_50_scaffolds.fasta.fai sorted_dh1.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --fais > synteny_all_haps.synteny_blocks.sorted.tsv
+python denovo_synteny_block_stats.py --tsv synteny_all_haps.synteny_blocks.tsv --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai
 
-# Generate the files needed for plotting with gggenomes
-python format_blocks_gggenomes.py --fai dh1_50_scaffolds.fasta.fai sorted_dh1.fasta.fai sorted_nh2.fasta.fai sorted_nh1.fasta.fai --prefix synteny_all_haps --blocks synteny_all_haps.synteny_blocks.sorted.tsv --length 100 --colour dh1_50_scaffolds.fasta
+python sort_ntsynt_blocks.py --synteny_blocks synteny_all_haps.synteny_blocks.tsv --sort_order dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --fais > synteny_all_haps.synteny_blocks.sorted.tsv
+
+python format_blocks_gggenomes.py --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --prefix synteny_all_haps --blocks synteny_all_haps.synteny_blocks.sorted.tsv --length 100 --colour dh1_50_scaffolds.fasta
 
 cat synteny_all_haps.links.tsv  | mlr --tsv sort -f strand -n block_id > synteny_all_haps.links.sorted.tsv && mv synteny_all_haps.links.sorted.tsv synteny_all_haps.links.tsv
 cat synteny_all_haps.sequence_lengths.tsv | mlr --tsv sort -f seq_id > synteny_all_haps.sequence_lengths.sorted.tsv && mv synteny_all_haps.sequence_lengths.sorted.tsv synteny_all_haps.sequence_lengths.tsv
 
 #run R script to generate plots
-Rscript plot_synteny_blocks_gggenomes.R -s synteny_all_haps.sequence_lengths.tsv -l synteny_all_haps.links.tsv --scale 200000000 --p synteny_all_haps
+Rscript plot_synteny_blocks_gggenomes.R -s synteny_all_haps.sequence_lengths.tsv -l synteny_all_haps.links.tsv --scale 50000000 --p synteny_all_haps
