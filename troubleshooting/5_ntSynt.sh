f53eeb53f0aa0d43b725166414e413d3d4e3d811
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-04-03 20:53:14 +0200

    Update 5_ntSynt.sh

--- troubleshooting/5_ntSynt.sh
+++ troubleshooting/5_ntSynt.sh
@@ -11,11 +11,14 @@ conda activate ntsynt
 
 ntSynt dh1_50_scaffolds.fasta sorted_dh2.fasta sorted_nh1.fasta sorted_nh2.fasta -p synteny_all_haps -t 24 -d 30
 
-python denovo_synteny_block_stats.py --tsv synteny_all_haps.synteny_blocks.tsv --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai
+python denovo_synteny_block_stats.py --tsv synteny_all_haps.synteny_blocks.tsv --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai \
+sorted_nh1.fasta.fai sorted_nh2.fasta.fai
 
-python sort_ntsynt_blocks.py --synteny_blocks synteny_all_haps.synteny_blocks.tsv --sort_order dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --fais > synteny_all_haps.synteny_blocks.sorted.tsv
+python sort_ntsynt_blocks.py --synteny_blocks synteny_all_haps.synteny_blocks.tsv --sort_order dh1_50_scaffolds.fasta.fai \
+sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --fais > synteny_all_haps.synteny_blocks.sorted.tsv
 
-python format_blocks_gggenomes.py --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai --prefix synteny_all_haps --blocks synteny_all_haps.synteny_blocks.sorted.tsv --length 100 --colour dh1_50_scaffolds.fasta
+python format_blocks_gggenomes.py --fai dh1_50_scaffolds.fasta.fai sorted_dh2.fasta.fai sorted_nh1.fasta.fai sorted_nh2.fasta.fai \
+--prefix synteny_all_haps --blocks synteny_all_haps.synteny_blocks.sorted.tsv --length 100 --colour dh1_50_scaffolds.fasta
 
 cat synteny_all_haps.links.tsv  | mlr --tsv sort -f strand -n block_id > synteny_all_haps.links.sorted.tsv && mv synteny_all_haps.links.sorted.tsv synteny_all_haps.links.tsv
 cat synteny_all_haps.sequence_lengths.tsv | mlr --tsv sort -f seq_id > synteny_all_haps.sequence_lengths.sorted.tsv && mv synteny_all_haps.sequence_lengths.sorted.tsv synteny_all_haps.sequence_lengths.tsv
