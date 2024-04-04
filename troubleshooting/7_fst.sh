5fc20681e043e7692fae18373b0cb11014b8b170
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-04-04 17:58:23 +0200

    Update 7_fst.sh

--- troubleshooting/7_fst.sh
+++ troubleshooting/7_fst.sh
@@ -1,1 +1,16 @@
-bin
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name fst_analysis
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=48:00:00
+#SBATCH -o fst.%j.out
+#SBATCH -A lp_svbelleghem
+
+#Step 1: map 100 resequencing short reads to reference primary genome (Dudzele and Nieuwpoort)
+
+#Step 2: Calculate genotypes from 100 bam files, 1 VCF file with VCFtools
+
+#Step 3: Strip VCF file into calls file (Simon Martin)
+
+#Step 4: Calculate Fst values from the valls file, determine blocks of highest Fst values for identifying inversions
